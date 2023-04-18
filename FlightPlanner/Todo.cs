2e5eccaf9ed1adb1561051f6cffbe050eb2148ac
Zyu <diamondlv42@gmail.com>
2023-04-19 00:01:01 +0300

    added todo for tomorrow

---
+++ FlightPlanner/Todo.cs
@@ -0,0 +1,16 @@
+﻿namespace FlightPlanner
+{
+    public class Todo
+    {
+        // TODO:
+        // - create extension methods, for example:
+        //      > string.Normalize() -> stirng.ToLower().Trim()
+        //      > a bunch of verification stuff, get that out of FlightStorage!
+        // - clean up imports
+        // - go through controllers, seperate logic into smaller methods
+        // - make basic auth less annoying
+        // - make a constructor for all models
+        // - remove weather forecast, what even is that
+        // - clean up everything
+    }
+}
